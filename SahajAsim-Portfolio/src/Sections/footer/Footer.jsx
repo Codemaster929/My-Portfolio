854d89028547137be7fa49a958198a77b182aa50
Sahaj Asim <mrx6909@gmail.com>
2026-03-23 00:35:42 +0530

    footer made

--- SahajAsim-Portfolio/src/Sections/footer/Footer.jsx
+++ SahajAsim-Portfolio/src/Sections/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='mt-15 bg-black/50 backdrop-blur-xs font-bold text-xl  border-t-1 border-white text-white h-20 w-full m-auto p-5 flex flex-row-reverse gap-2'>
+    <div className='mt-15 bg-black/50 backdrop-blur-xs font-bold text-xl  border-t border-white text-white h-20 w-full m-auto p-5 flex flex-row-reverse gap-2'>
     <h1>  Designed by sahaj asim </h1>
     </div>
   )
