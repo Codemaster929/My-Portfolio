5b241e3214fdba8c9aaca347a32065f8e7cdcdf8
Sahaj Asim <mrx6909@gmail.com>
2026-03-24 00:22:40 +0530

    added navigation and created all the sections

--- SahajAsim-Portfolio/src/Sections/Contactsection/ContactCard.jsx
+++ SahajAsim-Portfolio/src/Sections/Contactsection/ContactCard.jsx
@@ -5,10 +5,10 @@ const ContactCard = () => {
     
     <div className='flex flex-col '>
       <div className='flex gap-5 '>
- <input type="text" className='h-12 w-92 border-1 rounded bg-slate-950/60 border-gray-700 p-2' placeholder='Name' />
-      <input type="text"  className='h-12 w-93 border-1 rounded  bg-slate-950/60 border-gray-700 p-2' placeholder='Email'/>
+ <input type="text" className='h-12 w-92 border rounded bg-slate-950/60 border-gray-700 p-2' placeholder='Name' />
+      <input type="text"  className='h-12 w-93 border rounded  bg-slate-950/60 border-gray-700 p-2' placeholder='Email'/>
       </div>
-     <textarea className='h-50 resize-none mt-2 w-190 border-1 rounded  bg-slate-950/60 border-gray-700 p-4 align-top' placeholder='Drop a note with any website feedback or career opportunities, or just say hi. Where are you from? 😊 flex flex-wrap'></textarea>
+     <textarea className='h-50 resize-none mt-2 w-190 border rounded  bg-slate-950/60 border-gray-700 p-4 align-top' placeholder='Drop a note with any website feedback or career opportunities, or just say hi. Where are you from? 😊 flex flex-wrap'></textarea>
 
      <button className='bg-gray-200 text-black flex rounded font-bold items-center gap-3 justify-center w-190 mt-2 h-10 active:scale-95 '>Send Message <img className='h-5 w-5' src={messageicon} alt="" /></button>
     </div>
--- SahajAsim-Portfolio/src/Sections/Projectsection/ProjectCards.jsx
+++ SahajAsim-Portfolio/src/Sections/Projectsection/ProjectCards.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const ProjectCards = () => {
   return (
     <>
-     <div className='h-80 border-1 border-gray-800 bg-slate-950/60 w-60 flex flex-col gap-3 p-2 rounded-xl hover:scale-102 transition-transform cursor-pointer'>
+     <div className='h-80 border border-gray-800 bg-slate-950/60 w-60 flex flex-col gap-3 p-2 rounded-xl hover:scale-102 transition-transform cursor-pointer'>
       <img className='h-30 w-55 mx-auto rounded-xl object-cover '
        src="https://images.unsplash.com/photo-1772289238830-2464c7f0e708?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="img" />
       <h3 className='font-bold text-xl '>proj_name</h3>
       <p className='text-gray-300 text-xs'>Lorem ipsum dolor sit amet. ipsum, dolor sit amet consectetur adipisicing elit. Autem natus unde quos consequuntur optio!</p>
-      <div className='flex gap-2 text-white font-semibold flex flex-wrap '>
+      <div className='flex gap-2 text-white font-semibold  flex-wrap '>
       <h4 className='bg-gray-600 rounded px-1 border-none text-xs  flex items-center '>React</h4>
       <h4  className='bg-gray-600 rounded px-1 border-none text-xs  flex items-center '>Tailwind</h4>
       <h4 className='bg-gray-600 rounded px-1 border-none text-xs  flex items-center '>html</h4>
